28b69fe15502673931abad54b9d918d2cacb0af7
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-11-04 15:22:35 +0300

    add users app for project with urls, serializers, models and UserViewSet

---
+++ users/models.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Почта",
+        help_text="Укажите почту"
+    )
+    phone = models.CharField(
+        max_length=35,
+        verbose_name="Телефон",
+        help_text="Укажите номер телефона",
+        **NULLABLE
+    )
+    city = models.CharField(
+        max_length=50,
+        verbose_name="Город",
+        help_text="Укажите свой город",
+        **NULLABLE
+    )
+
+    def __str__(self):
+        return self.email
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
